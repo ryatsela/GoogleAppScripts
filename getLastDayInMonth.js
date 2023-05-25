d7193397f499d71fc050c4de883dcd10b7458ea5
Roman <65341707+ryatsela@users.noreply.github.com>
2023-05-25 16:46:57 +0200

    Delete getLastDayInMonth.js

--- getLastDayInMonth.js
+++
@@ -1,9 +0,0 @@
-const getLastDayInMonth = () => {
-  const now = new Date()
-  const lastDayInMonth = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    0
-  ).getDate()
-  return lastDayInMonth
-}
